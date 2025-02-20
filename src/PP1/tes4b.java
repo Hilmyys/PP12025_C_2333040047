209f484e3199a198b723875d12c4a96a7848f618
hilmyys <hilmiyr123@gmail.com>
2025-02-20 20:01:22 +0700

    sudah

--- src/PP1/tes4b.java
+++ src/PP1/tes4b.java
@@ -7,7 +7,7 @@ public class tes4b {
 		Node n2 = new Node(3);
 		Node n3 = new Node(5);
 		Node n4 = new Node(7);
-		Node n5 = new Node(8);
+		Node n5 = new Node(9);
 		
 		n1.setNext(n2);
 		n2.setNext(n3);
